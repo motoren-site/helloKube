af01314dd500ec559bf3b16133904c086160027e
Krister <kv@miles.no>
2023-10-28 16:19:15 +0200

    Added endpoint

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello Kube!');
 });
 
+app.get('/hallais', (req, res) => {
+    res.send('Hallais Kube!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
